96f2a823e53c639ff69577872a01d44a44c61520
BartuO <bartuokan1@gmail.com>
2023-11-04 15:50:45 -0600

    add CloseButton to addClothingScreen

--- Client/Pages/addClothingScreen.js
+++ Client/Pages/addClothingScreen.js
@@ -11,11 +11,8 @@ export default function AddClothingScreen({ visible, onRequestClose }) {
                 onRequestClose={onRequestClose}
             >
                 <View style ={styles.mainView}>
-                    <Text style>
-                        Under Construction
-                    </Text>
-                    <TouchableOpacity onPress={onRequestClose}>
-                        <Icon name="times" size={24} color="black" /> 
+                    <TouchableOpacity style = {styles.button} onPress={onRequestClose}>
+                        <Text style = {styles.buttonText}>X</Text> 
                     </TouchableOpacity>
                 </View>
             </Modal>
@@ -38,5 +35,21 @@ const styles = StyleSheet.create({
         width: "100%",
         backgroundColor: "#b2f7d1",
         borderRadius: 20
-    }
+    },
+    button: {
+        marginTop: "2%",
+        marginLeft: "90%",
+        width: 25,
+        height: 25,
+        borderRadius: 40, // Makes it round
+        backgroundColor: '#5be398', // Customize the button color
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        marginLeft: 1,
+        fontSize: 15,
+        color: '#b2f7d1',
+        lineHeight: 20
+    },
 })
