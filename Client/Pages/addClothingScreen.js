ae957ae125abfe512e55ce67b2c99dd2dd4fcd0a
BartuO <bartuokan1@gmail.com>
2023-12-01 18:05:06 -0700

    adding photo using camera implemented

--- Client/Pages/addClothingScreen.js
+++ Client/Pages/addClothingScreen.js
@@ -1,12 +1,60 @@
-import { Text, View, Modal, StyleSheet, TouchableOpacity, Icon } from "react-native";
-//import { Camera, useCameraDevices } from "react-native-vision-camera";
+import { Text, View, Modal, StyleSheet, TouchableOpacity, Icon, Image } from "react-native";
+import React, { useState } from 'react';
+import * as ImagePicker from "expo-image-picker";
+import placeholderImage from "../assets/photoPlaceholder.png";
+
 
 export default function AddClothingScreen({ visible, onRequestClose }) {
 
+    const [showOptions, setShowOptions] = useState(false);
 
- 
+    const [image, setImage] = useState(placeholderImage);
+
+
+    const openCamera = async() => {
+        try{
+            await ImagePicker.requestCameraPermissionsAsync();
+            let result = await ImagePicker.launchCameraAsync({
+                cameraType: ImagePicker.CameraType.back,
+                aspect: [1,1],
+                quality: 1,
+            });
+
+            if (!result.canceled){
+                await saveImage(result.assets[0].uri)
+            }
+        } catch (error){
+
+        }
+    }
+
+    const openLib = async() => {
+        const result = await launchImageLibrary();
+        setImgUrl(result?.assets[0]?.uri);
+    }
+
+
+    const openOptions = () => {
+        setShowOptions(true);
+    };
+
+    const closeOptions = () => {
+        setShowOptions(false);
+    };
+
+    const saveImage = async(image) => {
+        try{
+            setImage({ uri :image});
+            closeOptions();
+
+        } catch (error){
+            throw error;
+        }
+
+
+    }
+
     return (
-        
         <View>
             <Modal
                 animationType="slide"
@@ -14,29 +62,38 @@ export default function AddClothingScreen({ visible, onRequestClose }) {
                 visible={visible}
                 onRequestClose={onRequestClose}
             >
-                <View style ={styles.mainView}>
-                    <TouchableOpacity style = {styles.button} onPress={onRequestClose}>
-                        <Text style = {styles.buttonText}>x</Text> 
+                <View style={styles.mainView}>
+                    <TouchableOpacity style={styles.button} onPress={() => {setImage(placeholderImage); onRequestClose();}}>
+                        <Text style={styles.buttonText}>x</Text>
                     </TouchableOpacity>
 
-                    <TouchableOpacity style = {styles.addPhotoContainer}>
-                        <Text style = {styles.addPhotoText}> + </Text> 
+                    <TouchableOpacity style={styles.addPhotoContainer} onPress={openOptions}>
+                        <Image resizeMode="cover" style = {styles.img} source={image} />
                     </TouchableOpacity>
 
-                
-
+                    {/* Options popup */}
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={showOptions}
+                        onRequestClose={closeOptions}
+                    >
+                        <View style={styles.optionsView}>
+                            <TouchableOpacity style={styles.optionButton} onPress = {openLib}>
+                                <Text style={styles.optionButtonText}>Choose from library</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.optionButton} onPress = {() => openCamera()}>
+                                <Text style={styles.optionButtonText}>Capture using camera</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.optionButton} onPress={closeOptions}>
+                                <Text style={styles.optionButtonText}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </Modal>
                 </View>
             </Modal>
-            
-
-
         </View>
-
-
-
-
-    )
-
+    );
 }
 
 const styles = StyleSheet.create({
@@ -75,9 +132,34 @@ const styles = StyleSheet.create({
         alignItems: "center",
         backgroundColor: "#cadbd2"
     },
-    addPhotoText:{
-        fontSize: 55,
-        color: "white"
-
-    }
+    img:
+        {
+        width: "100%",
+        height: "100%",
+        alignSelf: "center",
+        borderRadius: 2,
+    },
+    optionsView: {
+        marginTop: "60%",
+        height: "40%",
+        width: "100%",
+        backgroundColor: "#fff",
+        borderTopEndRadius: 20,
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    optionButton: {
+        marginTop: 10,
+        width: "80%",
+        height: 40,
+        borderRadius: 4,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#99f2c1",
+    },
+    optionButtonText: {
+        fontSize: 16,
+        color: "white",
+    },
 })
